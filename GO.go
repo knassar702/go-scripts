8272150082a0dad5fca4e2975a21c1e6c9b34483
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2020-07-30 18:54:34 +0200

    Add files via upload

---
+++ GO.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "os"
+    "bufio"
+    "sync"
+    )
+
+func main() {
+    sc := bufio.NewScanner(os.Stdin)
+    var wg sync.WaitGroup
+    for sc.Scan(){
+        domain := sc.Text()
+        wg.Add(1)
+        go func(){
+            defer wg.Done()
+            _,err := net.LookupHost(domain)
+            if err != nil {
+                return
+            }
+            fmt.Println(domain)
+        }()
+    }
+    wg.Wait()
+}
